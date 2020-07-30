0cd56a6fffdd01e819a2e521e7b4b85475b44e44
Thiago Guimaraes <thiagoguimaraes.dev@gmail.com>
2020-07-30 13:32:07 -0300

    better questao 1

--- Lista2/questao_1.py
+++ Lista2/questao_1.py
@@ -2,51 +2,37 @@
 # n = lista de objetos
 # i = iterador (algoritmo eh recursivo)
 # objeto = [p, v], sendo p = peso e v = valor
-final_array_indexes = set()
-def greedy_knapsack(P, n, i = -1): # LETRA A
-  global final_array_indexes
+def greedy_knapsack(P, n, i = -1, final_array = []): # LETRA A
   if i == -1: # setar iterador na primeira chamada conforme tamanho do array de itens
     i = max((len(n) - 1), 0)
  
   if i == 0 or P == 0: # caso base
-    return 0
+    return [0, final_array]
 
   # maximo entre dois casos:
   if n[i][0] > P: # peso do item > capacidade do knapsack - nao podemos incluir
     return greedy_knapsack(P, n, i-1)
   else: # maximo entre dois casos:
     # item atual incluido e item atual nao incluido
-    included = n[i][1] + greedy_knapsack(P-n[i][0], n, i-1) # valor maximo quando incluido este item
-    not_included = greedy_knapsack(P, n, i-1) # valor maximo quando nao incluido
+    included_call = greedy_knapsack(P-n[i][0], n, i-1, final_array + [n[i]])
+    included = n[i][1] + included_call[0] # valor maximo quando incluido este item
+    not_included_call = greedy_knapsack(P, n, i-1, final_array) 
+    not_included = not_included_call[0] # valor maximo quando nao incluido
 
     if included > not_included:
-      final_array_indexes = final_array_indexes.union({i})
-      return included
+      return [included, included_call[1]]
     else:
-      return not_included
+      return not_included_call
 
-def get_knapsack_array(P, n, algo = 'greedy'):
-  final_array = []
-  if algo == 'greedy':
-    greedy_knapsack(P, n)
-  elif algo == 'dp':
-    dp_knapsack(P, n)
-
-  for i in final_array_indexes:
-    final_array.append(n[i])
-  return final_array
-
-coisas = [[10, 60], [20, 100], [30, 120]] 
-P = 50
-print(greedy_knapsack(P, coisas)) # retorna valor dentro do knapsack 
-print(get_knapsack_array(P, coisas, 'greedy')) # retorna o subconjunto dos objetos - passandro greedy pegamos com algoritmo guloso
+coisas = [[10, 60], [20, 100], [30, 120], [80, 1000], [20, 100]] 
+P = 80
+print(greedy_knapsack(P, coisas)) # retorna array com valor dentro do knapsack e array dos objetos dentro do knapsack 
 
 # Complexidade: O(2^n)
 
 dp = []
-def dp_knapsack(P, n, i = -1):
+def dp_knapsack(P, n, i = -1, final_array = []):
   global dp
-  global final_array_indexes
 
   if i == -1:
     i = len(n) - 1
@@ -57,24 +43,23 @@ def dp_knapsack(P, n, i = -1):
   if dp[i][P] != None: return dp[i][P] # aproveitar a memoizacao de operacoes
 
   if i == 0 or P == 0: # caso base
-    result = 0
+    result = [0, final_array]
   elif n[i][0] > P:
-    result = dp_knapsack(P, n, i-1)
+    result = dp_knapsack(P, n, i-1, final_array)
   else:
-    included = n[i][1] + dp_knapsack(P - n[i][0], n, i-1)
-    not_included = dp_knapsack(P, n, i-1)
+    included_call = dp_knapsack(P - n[i][0], n, i-1, final_array + [n[i]])
+    included = n[i][1] + included_call[0]
+    not_included_call = dp_knapsack(P, n, i-1, final_array)
+    not_included = not_included_call[0]
 
     if included > not_included:
-      final_array_indexes = final_array_indexes.union({i})
-      result = included
+      result = [included, included_call[1]]
     else:
-      result = not_included
+      result = not_included_call
   dp[i][P] = result
   return result
 
-final_array_indexes.clear()
 print(dp_knapsack(P, coisas))
-print(get_knapsack_array(P, coisas, 'dp')) # retorna o subconjunto dos objetos - passandro dp pegamos com algoritmo de programacao dinamica
 
 # Complexidade Tempo: O(n*P) - n: numero de elementos, P: capacidade desejada
 # Complexidade Espaco: O(n*P) - matriz auxiliar criada
