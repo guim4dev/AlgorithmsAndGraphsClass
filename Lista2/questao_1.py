9b5fc27474cb59c434e41f8766bd7bb95003dfa1
Thiago Guimarães <45672975+guim4dev@users.noreply.github.com>
2020-08-02 18:32:46 -0300

    fix dp_knapsack

--- Lista2/questao_1.py
+++ Lista2/questao_1.py
@@ -31,35 +31,51 @@ print(greedy_knapsack(P, coisas)) # retorna array com valor dentro do knapsack e
 # Complexidade: O(2^n)
 
 dp = []
-def dp_knapsack(P, n, i = -1, final_array = []):
+def dp_knapsack(P, n, i = -1):
   global dp
-
   if i == -1:
-    i = len(n) - 1
+    i = len(n)
 
   if dp == []: # montar tabela de Capacidades X Itens
-    dp = [[None for x in range(P+1)] for y in range(len(n))] # montar tabela de Capacidades X Itens
-
-  if dp[i][P] != None: return dp[i][P] # aproveitar a memoizacao de operacoes
+    dp = [[0 for x in range(P+1)] for y in range(len(n)+1)] # montar tabela de Capacidades X Itens
 
   if i == 0 or P == 0: # caso base
-    result = [0, final_array]
-  elif n[i][0] > P:
-    result = dp_knapsack(P, n, i-1, final_array)
+    result = 0
+  elif dp[i-1][P] != 0: # aproveitar a memoizacao de operacoes
+    return dp[i-1][P]
+  elif n[i-1][0] > P:
+    result = dp_knapsack(P, n, i-1)
   else:
-    included_call = dp_knapsack(P - n[i][0], n, i-1, final_array + [n[i]])
-    included = n[i][1] + included_call[0]
-    not_included_call = dp_knapsack(P, n, i-1, final_array)
-    not_included = not_included_call[0]
+    included = n[i-1][1] + dp_knapsack(P - n[i-1][0], n, i-1)
+    not_included = dp_knapsack(P, n, i-1)
 
-    if included > not_included:
-      result = [included, included_call[1]]
-    else:
-      result = not_included_call
+    result = max(included, not_included)
   dp[i][P] = result
-  return result
+  return(result)
 
-print(dp_knapsack(P, coisas))
+def get_dp_knapsack_array(P, n):
+  result = dp_knapsack(P, n)
+  res = result
+  items = []
+  p = P 
+  for i in range(len(n), 0, -1): 
+    if res <= 0: 
+      break
+    # either the result comes from the 
+    # top (K[i-1][w]) or from (val[i-1] 
+    # + K[i-1] [w-wt[i-1]]) as in Knapsack 
+    # table. If it comes from the latter 
+    # one/ it means the item is included. 
+    if res == dp[i - 1][p]: 
+      continue
+    else: 
+      # Item incluido 
+      items.append(n[i-1])
+      res = res - n[i - 1][1] 
+      p = p - n[i - 1][0]
+  return(result, items) 
+
+print(get_dp_knapsack_array(P, coisas))
 
 # Complexidade Tempo: O(n*P) - n: numero de elementos, P: capacidade desejada
 # Complexidade Espaco: O(n*P) - matriz auxiliar criada
